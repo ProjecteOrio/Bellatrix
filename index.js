7c5d73da4180cfdb88f27df51aa218242f18e29a
ProjecteOrio <projecte.orio@gmail.com>
2019-08-19 21:47:36 +0200

    clean demo

--- index.js
+++ index.js
@@ -2,20 +2,7 @@ let app  = require('express')();
 let http = require('http').Server(app);
 let io   = require('socket.io')(http);
 
-io.on('connection', (socket) => {
-    socket.on('disconnect', function(){
-        io.emit('users-changed', {user: socket.nickname, event: 'left'});   
-      });
-     
-      socket.on('set-nickname', (nickname) => {
-        socket.nickname = nickname;
-        io.emit('users-changed', {user: nickname, event: 'joined'});    
-      });
-      
-      socket.on('add-message', (message) => {
-        io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
-      });
-});
+io.on('connection', (socket) => { });
 
 var port = process.env.PORT || 3001;
 
