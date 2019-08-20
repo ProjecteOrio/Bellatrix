d2d7d7c4684cf9ce852cccd2b4ef43f8c054f03d
ProjecteOrio <projecte.orio@gmail.com>
2019-08-20 15:33:17 +0200

    fucking socket versions

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-let app  = require('express')();
-let http = require('http').Server(app);
-let io   = require('socket.io')(http);
+let socket = require('socket.io');
+let http = require('http');
+let server = http.createServer();
+io = socket.listen(server);
 
-io.on('connection', (socket) => { });
+io.on('connection', (connection) => { 
+
+  console.log('['+connection.id+']' + ' new-connection');
+
+  connection.on('disconnect', function() {
+    //io.emit('broadcast', {user: name, event: 'left'});
+  });
+
+  connection.on('user-connect', function (name) {
+    console.log('['+connection.id+']' + ' user-connect: ', name);
+    connection.name = name;
+    io.emit('broadcast', {user: name, event: 'joined'});
+  });
+
+  connection.on('add-message', (message) => {
+    console.log('['+connection.id+']' + ' add-message: ', message);
+    io.emit('message', {text: message.text,
+                        from: connection.eventNames,
+                        created: new Date() });
+  });
+
+});
 
 var port = process.env.PORT || 3001;
 
-http.listen(port, function () {
+server.listen(port, function () {
   console.log('open http://localhost:' + port);
 });
